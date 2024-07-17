bd527e4038c8bcc24aeb24da3e759a8cb64cf27c
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-17 13:56:37 +0000

    adding the structure to the main.h

--- main.h
+++ main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H (main)
+typedef struct datatype
+{
+    char *letra;
+    int (*funcion)(char *arg);
+} datatype;
 int _putchar(char c);
 int _printf(const char *format, ...);
 #endif /* DOG_H */
